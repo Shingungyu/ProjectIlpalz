6509ba8548b3a610456a150a911e46d1543038d4
hansb123a <epic7ebung@gmail.com>
2025-11-25 18:43:05 +0900

    Script res
    reset

--- Assets/P_Locale.cs
+++ Assets/P_Locale.cs
@@ -13,7 +13,7 @@ public class P_Locale : MonoBehaviour
     {
         originalScale = transform.localScale;
 
-        transform.DOMoveX(4f, 10f) //이동속도,거리
+        transform.DOMoveX(7f, 2f) //절대좌표,해당좌표에 도달하기까지 걸리는시간 순서 //
             .SetLoops(-1, LoopType.Yoyo)//-1은 반복시키기, 값이 아닌 yoyo는 무한루프
             .OnStepComplete(() =>
             {
@@ -22,7 +22,21 @@ public class P_Locale : MonoBehaviour
                     -transform.localScale.x,
                     transform.localScale.y,
                     transform.localScale.z
+
+                    
                 );
             });
+
+        JumpLoop();
     }
+    void JumpLoop()
+    {
+        Sequence jumpSeq = DOTween.Sequence();
+
+        jumpSeq.Append(transform.DOMoveY(transform.position.y + 2f, 0.3f)) // 위로
+                .Append(transform.DOMoveY(transform.position.y, 0.3f))     // 아래로
+                .SetDelay(3f)
+                .SetLoops(-1);
+    }
+
 }
